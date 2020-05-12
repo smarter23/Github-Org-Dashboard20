1abb1225b64b3f7ddda592785713a7f9eff03d0b
Ananya <ananyaganesh01@gmail.com>
2020-05-12 18:31:36 +0530

    error review

--- src/components/View3/View3.js
+++ src/components/View3/View3.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { VictoryBoxPlot , VictoryChart, VictoryPolarAxis,
-    VictoryTheme ,VictoryLegend, VictoryScatter,VictoryContainer, VictoryStack, VictoryBar} from 'victory';
+    VictoryTheme ,VictoryLegend, VictoryScatter,VictoryContainer, VictoryStack, VictoryBar, VictoryGroup,VictoryArea} from 'victory';
 import { Card,Row,Col } from 'antd';
 
 
@@ -42,35 +42,62 @@ class View3 extends React.Component{
       }
     render(){
 
-      let y = {}
-      this.state.topcontributors.map( el => {
-        if (y[el.top_contributor] === undefined || y[el.top_contributor] === 0) {
-        y[el.top_contributor] = 1
-      } else {
-        y[el.top_contributor] = y[el.top_contributor] + 1
-      }
-    })
-    console.log(y)
+    //   let y = {}
+    //   this.state.topcontributors.map( el => {
+    //     if (y[el.top_contributor] === undefined || y[el.top_contributor] === 0) {
+    //     y[el.top_contributor] = 1
+    //   } else {
+    //     y[el.top_contributor] = y[el.top_contributor] + 1
+    //   }
+    // })
+    // console.log(y)
     
-    let d = {}
-    let {topcontributors} = this.state;
+    // let d = {}
+    // let {topcontributors} = this.state;
+    // for (var i = 0; i < topcontributors.length; i++) {
+    //   var datum = topcontributors[i];
+    //   if (!d[datum.top_contributor]) {
+    //       d[datum.top_contributor] = [];
+    //   }
+    //   d[datum.top_contributor].push(datum.repo_name);
+    // }
+
+    var stackos =[] 
+    const {topcontributors} = this.state;
     for (var i = 0; i < topcontributors.length; i++) {
-      var datum = topcontributors[i];
-      if (!d[datum.top_contributor]) {
-          d[datum.top_contributor] = [];
-      }
-      d[datum.top_contributor].push(datum.repo_name);
+      var stacko = <VictoryBar  data = {{x:topcontributors[i].top_contributor, y: 1}} key={topcontributors[i].top_contributor} />
+      stackos.push(stacko)
     }
-    console.log(d)
-    for (var el of Object.keys(d)) {
-      let data = []
-      d[el].forEach(el => {
-        // console.log(el)
-        data.push({x: el, y: 1})   
-        // console.log(data)             
-      })
-      const stack = (<VictoryBar data = {data} />)
-  }
+    console.log(stackos)
+    // const stackdata = topcontributors.length ? (
+    //   topcontributors.map(
+    //     data => {
+    //       return (
+    //         <VictoryBar  data = {{x:data.top_contributor, y: 1}} />
+    //       )
+    //     }
+    //   )
+    // ): (
+    //   <div> 0 </div>
+    // )
+    // var stack_data = this.state.topcontributors.map(data => {
+    //   console.log(data)
+    //   var stack = <VictoryBar  data = {{x:data.top_contributor, y: 1}} key={data.top_contributor} />
+    // })
+
+  //   var stacks = [] 
+  //   console.log(d)
+  //   for (var el of Object.keys(d)) {
+  //     let data = []
+  //     d[el].forEach(el => {
+  //       // console.log(el)
+  //       data.push({x: el, y: 1})   
+  //       // console.log(data)             
+  //     })
+  //     var stack = (<VictoryBar  data = {data} key={data.x} />)
+  //     stacks.push(stack)
+  //     console.log(stacks)
+  // }
 
         return(
           <Row>
@@ -78,11 +105,22 @@ class View3 extends React.Component{
             <Card style={{margin :20}}>
             <div>
               <h1 style={{textAlign:"center"}}>Top Contributors</h1>
-              <VictoryStack
-               colorScale={["#087e8b", "#c1839f", "gold"]}
-              >
-              {stack}
-              </VictoryStack>
+
+              <VictoryChart  width={400}>
+              {/* <VictoryGroup  style={{ data: { width: 15 } }}> */}
+                  <VictoryStack
+                  height={150}
+                  colorScale={"blue"}
+                  animate={{
+                    duration: 2000,
+                    onLoad: { duration: 1000 }
+                  }}
+                  >
+                  {/* {stackos} */}
+                  </VictoryStack>
+                {/* </VictoryGroup> */}
+              </VictoryChart>
+
             
             </div>
             </Card>
